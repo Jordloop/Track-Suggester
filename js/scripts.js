b6e08fb81d13302ca6d517eb7463292b566f2d45
Jordan Loop <jordloop@MacBook-Air.local>
2017-04-28 10:59:17 -0700

    Add functional business logic, foundation for interface logic in place.

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ $(function(){
   $("form#user-input").submit(function(event){
     event.preventDefault();
 
+//----------BUSINESSLOGIC----------
     var cSharp = 0
     var cssDesign = 0
     var rubyRails = 0
@@ -13,22 +14,88 @@ $(function(){
     var thirdAnswer = $("#thirdQuestion").val();
     var fourthAnswer = $("#fourthQuestion").val();
     var fifthAnswer = $("#fifthQuestion").val();
-
-    $(console.log(firstAnswer))
+    // FIRSTQUESTIONLOGIC
     if (
       firstAnswer === "Design") {
-      cssDesign ++
+        cssDesign ++
     } else if (
       firstAnswer === "C#") {
-      cSharp ++
+        cSharp ++
     } else if (
       firstAnswer === "Ruby") {
-      rubyRails ++
+        rubyRails ++
     } else {
       $(console.log("Error in if code"))
     };
-    $(console.log(firstAnswer))
     $(console.log("C# "+cSharp, "CSS "+cssDesign, "Ruby "+rubyRails));
-
+    // SECONDQUESTIONLOGIC
+    if (
+      secondAnswer === "Design") {
+        cssDesign ++
+    } else if (
+      secondAnswer === "C#") {
+        cSharp ++
+    } else if (
+      secondAnswer === "Ruby") {
+        rubyRails ++
+    } else {
+      $(console.log("Error in if code"))
+    };
+    $(console.log("C# "+cSharp, "CSS "+cssDesign, "Ruby "+rubyRails));
+    // THIRDQUESTIONLOGIC
+    if (
+      thirdAnswer === "Design") {
+        cssDesign ++
+    } else if (
+      thirdAnswer === "C#") {
+        cSharp ++
+    } else if (
+      thirdAnswer === "Ruby") {
+        rubyRails ++
+    } else {
+      $(console.log("Error in if code"))
+    };
+    $(console.log("C# "+cSharp, "CSS "+cssDesign, "Ruby "+rubyRails));
+    // FOURTHQUESTIONLOGIC
+    if (
+      fourthAnswer === "Design") {
+        cssDesign ++
+    } else if (
+      fourthAnswer === "C#") {
+        cSharp ++
+    } else if (
+      fourthAnswer === "Ruby") {
+        rubyRails ++
+    } else {
+      $(console.log("Error in if code"))
+    };
+    $(console.log("C# "+cSharp, "CSS "+cssDesign, "Ruby "+rubyRails));
+    // FIFTHQUESTIONLOGIC
+    if (
+      fifthAnswer === "Design") {
+        cssDesign ++
+    } else if (
+      fifthAnswer === "C#") {
+        cSharp ++
+    } else if (
+      fifthAnswer === "Ruby") {
+        rubyRails ++
+    } else {
+      $(console.log("Error in if code"))
+    };
+    $(console.log("C# "+cSharp, "CSS "+cssDesign, "Ruby "+rubyRails));
+//----------INTERFACELOGIC----------
+    if (
+      cSharp > cssDesign && cSharp > rubyRails) {
+        $(console.log("C# wins"))
+    } else if (
+      cssDesign > cSharp && cssDesign > rubyRails) {
+        $(console.log("cssDesign wins"))
+    } else if (
+      rubyRails > cSharp && rubyRails > cssDesign) {
+        $(console.log("cssDesign wins"))
+    } else {
+      $(console.log("Error in if code"))
+    };
   })
 })
